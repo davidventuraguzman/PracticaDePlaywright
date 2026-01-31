2b01fee0aded93761b26212e948b2d291e7d8d4b
davidventuraguzman <david.02.vg@gmail.com>
2026-01-31 17:09:48 -0500

    feat: logic for orange login

--- tests/Practica/Login.spec.ts
+++ tests/Practica/Login.spec.ts
@@ -22,20 +22,42 @@ test("testOrange", async ({ page }) => {
   await page.goto("https://www.orangehrm.com/");
 
   await page.getByRole("button", { name: "Book a Free Demo" }).click();
+
   await page.getByRole("textbox", { name: "Full Name" }).fill("David");
   await page
     .getByRole("textbox", { name: "Email" })
     .fill("david.02.vg@gmail.com");
-  await page.getByRole("textbox", { name: "Contact" }).fill("123456789");
 
-  const arrayconutry = await page.getByRole("combobox", { name: "Country" });
+  await page.getByRole("textbox", { name: "Phone Number" }).fill("123456789");
 
-  const countries = await arrayconutry.allTextContents();
+  //logica para el country
+  const countryDropdown = page.locator("#Form_getForm_Country");
+  const count = await countryDropdown.locator("option").count();
 
-  const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+  const randomIndex = Math.floor(Math.random() * (count - 1)) + 1;
+  await countryDropdown.selectOption({ index: randomIndex });
 
-  const country = await page.getByRole("combobox", { name: "Country" });
-  await country.fill(randomCountry);
+  console.log("País seleccionado index:", randomIndex);
 
-  console.log(`País seleccionado: ${randomCountry}`);
+  await page.getByRole("textbox", { name: "Company Name" }).fill("Davicho");
+  await page.getByRole("textbox", { name: "Job Title" }).fill("Desarrollador");
+
+  //logica para number of employees
+  const numberOfEmployeesDropdown = page.locator("#Form_getForm_NoOfEmployees");
+  const countNumberOfEmployees = await numberOfEmployeesDropdown
+    .locator("option")
+    .count();
+
+  const randomIndexNumberOfEmployees =
+    Math.floor(Math.random() * (countNumberOfEmployees - 1)) + 1;
+  await numberOfEmployeesDropdown.selectOption({
+    index: randomIndexNumberOfEmployees,
+  });
+
+  console.log(
+    "Número de empleados seleccionado index:",
+    randomIndexNumberOfEmployees,
+  );
+
+  await page.pause();
 });
