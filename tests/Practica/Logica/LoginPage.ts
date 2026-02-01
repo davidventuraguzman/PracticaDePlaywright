cf8dcad24861839252d528f367fdb4b45b8ed7d6
davidventuraguzman <david.02.vg@gmail.com>
2026-01-31 21:07:34 -0500

    feat: separate logic

--- tests/Practica/Logica/LoginPage.ts
+++ tests/Practica/Logica/LoginPage.ts
@@ -1,3 +1,106 @@
+import { Page, Locator } from "@playwright/test";
+
+export const usuariosOrange = [
+  {
+    nombre: "David Ventura",
+    email: "david.rara@gmail.com",
+    telefono: "999111222",
+    empresa: "Empresa Uno",
+    cargo: "QA",
+    mensaje: "Hola mundo 1",
+  },
+  {
+    nombre: "Anlly",
+    email: "anlly@gmail.com",
+    telefono: "999333444",
+    empresa: "Empresa Dos",
+    cargo: "Dev",
+    mensaje: "Hola mundo 2",
+  },
+];
+
 export class LoginPage {
-  constructor() {}
+  readonly page: Page;
+  //selectores vacios
+  readonly orangeBookDemoBtn: Locator;
+  readonly orangeFullName: Locator;
+  readonly orangeEmail: Locator;
+  readonly orangePhone: Locator;
+  readonly orangeCountryDropdown: Locator;
+  readonly orangeCompanyName: Locator;
+  readonly orangeJobTitle: Locator;
+  readonly orangeEmployeesDropdown: Locator;
+  readonly orangeMessage: Locator;
+  readonly orangeContactSalesBtn: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    //selectores llenos
+    this.orangeBookDemoBtn = page.getByRole("button", {
+      name: "Book a Free Demo",
+    });
+    this.orangeFullName = page.getByRole("textbox", { name: "Full Name" });
+    this.orangeEmail = page.getByRole("textbox", { name: "Email" });
+    this.orangePhone = page.getByRole("textbox", { name: "Phone Number" });
+    this.orangeCountryDropdown = page.locator("#Form_getForm_Country");
+    this.orangeCompanyName = page.getByRole("textbox", {
+      name: "Company Name",
+    });
+    this.orangeJobTitle = page.getByRole("textbox", { name: "Job Title" });
+    this.orangeEmployeesDropdown = page.locator("#Form_getForm_NoOfEmployees");
+    this.orangeMessage = page.getByRole("textbox", { name: "Your Message" });
+    this.orangeContactSalesBtn = page.getByRole("button", {
+      name: "Contact Sales",
+    });
+  }
+
+  async gotoOrange() {
+    await this.page.goto("https://www.orangehrm.com/");
+  }
+
+  async registrarOrange(
+    nombre: string,
+    email: string,
+    phone: string,
+    company: string,
+    jobTitle: string,
+    message: string,
+  ) {
+    await this.orangeBookDemoBtn.click();
+
+    await this.orangeFullName.fill(nombre);
+    await this.orangeEmail.fill(email);
+    await this.orangePhone.fill(phone);
+
+    // logica para el country
+    const countCountry = await this.orangeCountryDropdown
+      .locator("option")
+      .count();
+    const randomCountryIndex =
+      Math.floor(Math.random() * (countCountry - 1)) + 1;
+    await this.orangeCountryDropdown.selectOption({
+      index: randomCountryIndex,
+    });
+    console.log("País seleccionado index:", randomCountryIndex);
+
+    await this.orangeCompanyName.fill(company);
+    await this.orangeJobTitle.fill(jobTitle);
+
+    // logica para el numero de empleados
+    const countEmployees = await this.orangeEmployeesDropdown
+      .locator("option")
+      .count();
+    const randomEmployeesIndex =
+      Math.floor(Math.random() * (countEmployees - 1)) + 1;
+    await this.orangeEmployeesDropdown.selectOption({
+      index: randomEmployeesIndex,
+    });
+    console.log(
+      "Número de empleados seleccionado index:",
+      randomEmployeesIndex,
+    );
+    await this.orangeMessage.click();
+    await this.orangeMessage.fill(message);
+    await this.orangeContactSalesBtn.click({ force: true });
+  }
 }
--- tests/Practica/Login.spec.ts
+++ tests/Practica/Login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage, usuariosOrange } from "./Logica/LoginPage";
 
 test("test", async ({ page }) => {
   await page.goto("https://demo.realworld.io/#/");
@@ -18,52 +19,19 @@ test("test", async ({ page }) => {
   await page.pause();
 });
 
-test("testOrange", async ({ page }) => {
-  await page.goto("https://www.orangehrm.com/");
+test("Tests OrangeHRM - Registro Único", async ({ page }) => {
+  const usuario = usuariosOrange[0];
+  const loginPage = new LoginPage(page);
 
-  await page.getByRole("button", { name: "Book a Free Demo" }).click();
-
-  await page.getByRole("textbox", { name: "Full Name" }).fill("David");
-  await page
-    .getByRole("textbox", { name: "Email" })
-    .fill("david.02.vg@gmail.com");
-
-  await page.getByRole("textbox", { name: "Phone Number" }).fill("123456789");
-
-  //logica para el country
-  const countryDropdown = page.locator("#Form_getForm_Country");
-  const count = await countryDropdown.locator("option").count();
-
-  const randomIndex = Math.floor(Math.random() * (count - 1)) + 1;
-  await countryDropdown.selectOption({ index: randomIndex });
-
-  console.log("País seleccionado index:", randomIndex);
-
-  await page.getByRole("textbox", { name: "Company Name" }).fill("Davicho");
-  await page.getByRole("textbox", { name: "Job Title" }).fill("Desarrollador");
-
-  //logica para number of employees
-  const numberOfEmployeesDropdown = page.locator("#Form_getForm_NoOfEmployees");
-  const countNumberOfEmployees = await numberOfEmployeesDropdown
-    .locator("option")
-    .count();
-
-  const randomIndexNumberOfEmployees =
-    Math.floor(Math.random() * (countNumberOfEmployees - 1)) + 1;
-  await numberOfEmployeesDropdown.selectOption({
-    index: randomIndexNumberOfEmployees,
-  });
-
-  console.log(
-    "Número de empleados seleccionado index:",
-    randomIndexNumberOfEmployees,
+  await loginPage.gotoOrange();
+  await loginPage.registrarOrange(
+    usuario.nombre,
+    usuario.email,
+    usuario.telefono,
+    usuario.empresa,
+    usuario.cargo,
+    usuario.mensaje,
   );
-
-  await page
-    .getByRole("textbox", { name: "Your Message" })
-    .fill("Se logro la prueba");
-
-  await page.getByRole("button", { name: "Contact Sales" }).click();
 
   await page.pause();
 });
